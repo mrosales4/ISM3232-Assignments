81e402184e8b81b764b0cf1cc4affd2bb82862e6
Matthew A Rosales <matthewrosales@usf.edu>
2024-05-17 12:41:41 -0400

    Add output functionality to display bill, tip, and total.

--- cc_1.js
+++ cc_1.js
@@ -3,3 +3,13 @@ function calcTip(billAmount) {
 const tip = billAmount >= 50 && billAmount <= 300 ? billAmount * 0.15 : billAmount * 0.2;
 return tip;
 }
+
+// Display a message that includes the bill amount, the tip amount, and the total cost (bill plus tip).
+function displayTotal(billAmount) {
+const tip = calcTip(billAmount);
+const totalCost = billAmount + tip;
+console.log(`The bill was $${billAmount}, the tip was $${tip.toFixed(2)}, and the total value is $${totalCost.toFixed(2)}`);
+}
+//Answer chnages if bill price is changed 
+const bill = 275;
+displayTotal(bill);
